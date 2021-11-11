04a4b4f926b8bba50e16c5beb978717722667234
brandy-kay <brandyariana99@gmail.com>
2021-11-11 21:06:33 +0300

    fragment and activity set up

---
+++ app/src/main/java/com/kanyiakinyidevelopers/goals/ui/activities/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.kanyiakinyidevelopers.goals.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kanyiakinyidevelopers.goals.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
